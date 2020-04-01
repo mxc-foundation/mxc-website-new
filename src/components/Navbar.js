2c2246b5a0284bb7d303468ab038a871a97b26ce
stahldom <jeff@stahlnecker.me>
2020-04-01 15:59:53 +0000

    added images, modified style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import styles from '../css/navbar.module.css'
+import { graphql, useStaticQuery } from 'gatsby'
+import Image from 'gatsby-image'
+
+const getLogo = graphql`
+{
+    logo:file(relativePath: {eq:"mxclogo.png"}) {
+      childImageSharp {
+        fluid {
+            src
+        }
+      }
+    }
+  }
+  
+`  
+
 
 const Navbar = () => {
+    const data = useStaticQuery(getLogo)
+
     return (
         <div className={styles.navbarStart}>
             Navbar
+            <Image fluid={data.childImageSharp.fluid}></Image>
         </div>
     )
 }
